200bbdc17cee082e0dcb662a44437184dbec1919
Alice Kim <hyejik5@uci.edu>
2021-03-04 00:26:41 -0800

    Co-authored-by: Faustina Nyaung <fnyaung@gmail.com>

--- scratch.py
+++ scratch.py
@@ -5,7 +5,7 @@ import numpy as np
 import math
 import collections
 from tokenizer import Tokenizer
-from numpy import dot
+from numpy import append, dot
 from numpy.linalg import norm
 
 # ## lemitize
@@ -22,7 +22,7 @@ from bson.objectid import ObjectId
 
 
 myclient = MongoClient("mongodb://localhost:27017/")
-db = myclient['CS121_norm_100']
+db = myclient['CS121_norm_1000']
 tokenObj = Tokenizer()
 
 
@@ -32,43 +32,6 @@ def print_dict(cur_dict):
         print(key, cur_dict[key])
 
 
-# def get_token_tfidf(db, token): 
-#     """
-#     Looks for the token in the db and returns dictionay
-#     return : dict - { docid : tfidf ... }
-#     """
-#     db_dict = {}
-#     # text first char in token
-#     first_letter = token[0]
-#     # this is text[0]
-#     alpha_db = db[first_letter]
-#     doc_info = alpha_db.find_one({"_id":token})['doc_info']
-
-#     # getting all the doc info
-#     for doc in doc_info:
-#         db_dict[doc['uniqueID']] = doc['tf-idf']
-#         # print(doc['uniqueID'], doc['tf-idf'])
-
-#     return db_dict
-
-
-# def get_w_tfidf(token_dict):
-#     """
-#     Computes the normalized tfidf by dividing with query lengh
-#     return : dict | { docid : w_tfidf ... } 
-#     """
-#     query_len = 0
-    
-#     for key in token_dict:
-#         query_len = token_dict[key]**2
-#     query_len = math.sqrt(query_len)
-
-#     for key in token_dict:
-#         token_dict[key] = token_dict[key]/query_len
-
-#     return token_dict
-
-
 def get_query_vector(user_input):
     """
     for the query vector, normalizes and returns vector
@@ -76,6 +39,8 @@ def get_query_vector(user_input):
     returns : np.array(vector_list), user_input_unique
     """
     print(user_input)
+    # [alice fly faustina flys]
+    # [alice fly faustina fly]
     user_list = tokenObj.tokenize(user_input) # assume not a unique words allows dupe
     input_freq_dict = collections.Counter(user_list) ## assume its returning term tf weight 
     
@@ -83,10 +48,10 @@ def get_query_vector(user_input):
     user_input_unique = [] # keep track of the query vec word order
     normalized_list = []
 
-
     ## calculate tf weight 
     print("Start calculate tf weight")
     print(input_freq_dict)
+
     for key in input_freq_dict:
         tf_weight = get_query_weight(key, input_freq_dict[key])
         print(key , "weight :", tf_weight)
@@ -119,7 +84,7 @@ def get_query_vector(user_input):
 def get_query_weight(query, tfwt):
     """
     for the query weight 
-    param : query | string 
+    param : query | string , tfwt | int
     returns : query_weight | float
     """
     # text first char in query
@@ -127,28 +92,111 @@ def get_query_weight(query, tfwt):
 
     # this is text[0]
     alpha_db = db[first_letter]
-    df = alpha_db.find_one({"_id":query})['total']
-    print("total of", query, ": ", df)
+    # df = alpha_db.find_one({"_id":query})['total']
+    idf = alpha_db.find_one({"_id":query})['idf']
+    # print("total of", query, ": ", df)
 
     ## according to the db! ##
-    total_amt_doc = 100 
+    # total_amt_doc = 100 
     ## according to the db! ##
 
     # idf = math.log(total_amt_doc/df)
-    print("get_query_weight total_amt_doc :", total_amt_doc)
+    # print("get_query_weight total_amt_doc :", total_amt_doc)
     print("get_query_weight tfwt :", tfwt)
-    print("get_query_weight df :", df)
-    print("get_query_weight idf", math.log10(total_amt_doc/df))
-    weight = tfwt * math.log10(total_amt_doc/df)
+    # print("get_query_weight df :", df)
+    print("get_query_weight idf", idf)
+    weight = tfwt * idf 
     print("get_query_weight's weight : ", weight)
+
     return weight
 
 
 
+def document_weight_tag(user_input_unique_list):
+    """
+    for the query normalized values  
+    param : user_input_unique | string 
+    returns : doc_dic | string
+    """
+    word_doc_dict = {}
+    # {query_word : docdictionay, query_word : docdictionay}
+    docid_list = [[],[]]
+
+    # if the query is 2 words only
+    if len(user_input_unique_list) == 2:
+
+        # for query_word in user_input_unique_list:
+        for i in range(len(user_input_unique_list)):
+            # doc_dic = {origianlId : [normalized, tagscore]}
+            # doc_tag_noarmald = key = tagscore : value docid_list = []
+            # doc_tag_noarmal = {}
+            # if tagscore is not in doc_tag_noarmal.keys():
+            #     add into doc_tag_noarmal[tagscore] = docid
+            # else:
+            #     doc_tag_noarmal[tagscore] add into list of docid_list
+
+            # sort dictionay with key (tagscore):
+            # we get top 30 ->
+
+            doc_dict = {}
+
+            first_letter = user_input_unique_list[i][0]
+            alpha_db = db[first_letter]
+            docinfo = alpha_db.find_one({"_id":user_input_unique_list[i]})['doc_info'] 
+
+            # each word willl have a doc_dict 
+            for document in docinfo:
+                doc_dict[document['originalID']] = [document['normalized'], document['tagScore']] # [normalized, tagsore]
+                docid_list[i].append(document['originalID'])
+            
+            word_doc_dict[user_input_unique_list[i]] = doc_dict
+        
+        print_dict(word_doc_dict)
+
+        # docid that contains both words 
+        unique_id = set(docid_list[0]).intersection(set(docid_list[1]))
+        # word_doc_dict : {query_word : {origianlId : [normalized, tagscore]}, query_word : {origianlId : [normalized, tagscore]}}
+        # access all the document objects for 1 term
+        
+        # Faustina Code
+        # counter = 0
+        # minTagscore = 0
+        # topTagScore = []
+        # for i in range(len(user_input_unique_list)): 
+        #     # for every document in the query[i] (a word)   
+        #     for currDoc in word_doc_dict[user_input_unique_list[i]]:
+        #         currTagScore = word_doc_dict[user_input_unique_list[i]][currDoc][1]
+        #         if counter == 0:
+        #             # intialize minimum tag score / doc associated with minTagScore
+        #             minTagScore = word_doc_dict[user_input_unique_list[i]][currDoc][1]
+        #             minDoc = currDoc
+                
+        #         if counter > 30:
+        #             if currTagScore >= minTagScore:
+                        
+
+            
+            # print(f"i = {i}")
+            # print(word_doc_dict[user_input_unique_list[i]])
+
+        # for word_doc in word_doc_dict:
+        #     docdictionay = word_doc_dict[word_doc]
+                
+        
+        # for word_doc in word_doc_dict:
+        #     doc_dictionay = word_doc_dict[word_doc] # {origianlId : [normalized, tagscore]}
+        #     for norm_tag in doc_dictionay:
+        #         tag = norm_tag[1] # tag importance
+
+    # return doc_dict
+
+
+document_weight_tag(['fully','connectivity'])
+
 
 def document_weight(one_word):
     """
-    for the query weight 
+    for the query normalized values  
     param : user_input_unique | string 
     returns : doc_dic | string
     """
@@ -169,55 +217,51 @@ def document_weight(one_word):
 # key = [doc_idunion]: value_normalised=[[d1], [d2], [d3]]
 
 
-# def document_weight_muliple(user_input_unique):
-#     """
-#     for the query weight for multiple
-#     param : user_input_unique | list string 
-#     returns : doc_dic | doc_dic
-#     """
-#     doc_dic = {}
+def document_weight_muliple(user_input_unique):
+    """
+    for the query weight for multiple
+    param : user_input_unique | list string 
+    returns : doc_dic | doc_dic
+    """
+    doc_dic = {}
 
-#     idx_user_input = len(user_input_unique)
+    idx_user_input = len(user_input_unique)
 
-#     d_dict_list = [] # all document dictioniees in list key:normalized
-#     # we have to keep track of the  which word accordings to the normzlaied .
-#     # we need to put it in a double array, x=documents y=terms
-#     union_docid = []
+    d_dict_list = [] # all document dictioniees in list key:normalized
+    # we have to keep track of the  which word accordings to the normzlaied .
+    # we need to put it in a double array, x=documents y=terms
+    # user_input_unique[]
     
-#     for i in range(idx_user_input):
-#         # retrive all document in all quries 
-#         doc_dict = document_weight(user_input_unique[i]) 
-#         d_dict_list.append(doc_dict)
-#         if i == 0:
-#             union_docid += doc_dict.keys()
-#         else:
-#             union_docid = set(set(doc_dict.keys()).union(union_docid))
-    
-#     for docid in union_docid:
-#         for d_dict in d_dict_list:
+    for i in range(idx_user_input):
+        # retrive all document in all quries 
+        doc_dict = document_weight(user_input_unique[i]) # asumming top 30 tag_imp here 
+        d_dict_list.append(doc_dict) # asumming top 30 tag_imp
+
+    cosine_sim_list = [] # list of cosine sim of each doc & query 
+    # q_vec = []
+    # cosine_sim_list = cosine_similarity(q_vec, d_dict_list)
+
+    return cosine_sim_list
 
 
-#     return doc_dic
-
-
+# i was gonna json file according to each uniquedocid,
 
 # doc_vect = [doc2[normlized] , doc2[normalzied]]
-def cosine_simlarity(q_vec, d_dict, user_input_unique):
+def cosine_similarity(q_vec, d_dict):
     """
-    param : q_vec|np array, d_vec| list of list, user_input_unique
+    commputes cosine similarity for one word
+    param : q_vec|np array, d_vec| list of list
     return : list | cos_sim_list
     """
     cos_sim_list = []
 
-    if len(user_input_unique) == 1: 
-        # example cut 
-        # {3: 0.23201859645566503, 39: 0.08625382108082906}
-        for doc in d_dict:
-            # doc product of q_vec & 
-            # https://stackoverflow.com/questions/18424228/cosine-similarity-between-2-number-lists
-            d_vec = np.array(d_dict[doc])
-            cos_sim = np.dot(d_vec, q_vec)
-            cos_sim_list.append(cos_sim)
+    # example cut 
+    # {3: 0.23201859645566503, 39: 0.08625382108082906}
+    for doc in d_dict:
+        # doc product of q_vec & 
+        d_vec = np.array(d_dict[doc])
+        cos_sim = np.dot(d_vec, q_vec)
+        cos_sim_list.append(cos_sim)
 
     cos_sim_list.sort(reverse=True)
 
@@ -225,9 +269,43 @@ def cosine_simlarity(q_vec, d_dict, user_input_unique):
         cos_sim_list = cos_sim_list[:20]
     
     return cos_sim_list
+
+
+# def compute_score(cos_sim_list):
+#     score_list = []
+
+#     for cos_sim in cos_sim_list: # one doc
+#         score = #add all oof them 
+
+
+# # doc_vect = [doc2[normlized] , doc2[normalzied]]
+# def cosine_similarity(q_vec, d_dict, user_input_unique):
+#     """
+#     commputes cosine similarity 
+#     param : q_vec|np array, d_vec| list of list, user_input_unique
+#     return : list | cos_sim_list
+#     """
+#     cos_sim_list = []
+
+#     if len(user_input_unique) == 1: 
+#         # example cut 
+#         # {3: 0.23201859645566503, 39: 0.08625382108082906}
+#         for doc in d_dict:
+#             # doc product of q_vec & 
+#             # https://stackoverflow.com/questions/18424228/cosine-similarity-between-2-number-lists
+#             d_vec = np.array(d_dict[doc])
+#             cos_sim = np.dot(d_vec, q_vec)
+#             cos_sim_list.append(cos_sim)
+
+#     cos_sim_list.sort(reverse=True)
+
+#     if (len(cos_sim_list) > 20 ):
+#         cos_sim_list = cos_sim_list[:20]
+    
+#     return cos_sim_list
     
 
-
+"""
 enter1 = "cut" # 
 enter2 = "cut connectivity" # cut - doc id : 3 & 39 , connectivity - doc id: 3 & 52
 enter3 = "cut fully" # cut - doc id : 3 & 39 , fully - doc id: 3 & 7 & 92
@@ -240,8 +318,10 @@ q_vec , unique_list = get_query_vector(user_input)
 print("get_query_vector: ", q_vec , unique_list)
 doc_dict = document_weight(unique_list)
 print("document_weight: ", doc_dict)
-cos_sim_list = cosine_simlarity(q_vec, doc_dict, unique_list)
+cos_sim_list = cosine_similarity(q_vec, doc_dict, unique_list)
 print("cos sim list : ", cos_sim_list)
+"""
+
 
 # ## param : user_input | list of words after the tokenization function
 # def serach_query(db, user_input):
